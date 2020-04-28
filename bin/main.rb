aa0fd1792b6fc382a365638e5fcb03b987e48aa2
Hemant soni <sonihemant4741@gmail.com>
2020-04-29 01:51:25 +0530

    user_input

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,30 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+class Game
+  attr_accessor :board
+  def initialize
+    @board = []
+  end
+
+  def start
+    puts 'Hello! Welcome to Tic-Toc-Toe Game'
+    puts 'What is your name Player One?'
+    player1 = gets.chomp
+    puts 'What is your name Player Two?'
+    player2 = gets.chomp
+    puts "Let's Play!"
+    turn = 1.to_i
+    while turn < 10
+      if turn.odd?
+        puts "#{player1} choose no. 1 to 9"
+        # store input in board
+      else
+        puts "#{player2} choose no. 1 to 9"
+        # store input in board
+      end
+      turn += 1
+    end
+  end
+end
+
+tic = Game.new
+tic.start
