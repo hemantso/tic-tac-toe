77259049344c63dcccf1926219ec6b7cb0b1e312
Hemant soni <sonihemant4741@gmail.com>
2020-05-08 21:11:48 +0530

    test added for board.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,139 +1,73 @@
 #!/usr/bin/env ruby
-class Game
-  attr_accessor :board
+
+require_relative '../lib/board.rb'
+
+class User
   def initialize
-    @board = []
+    @game = Board.new
+    @board = @game.init_board(9)
   end
 
-  WIN_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [6, 4, 2],
-    [0, 4, 8]
-  ].freeze
+  def welcome
+    puts '==========TIC-TAC-TOE=========='
+    puts '-------------INSTRUCTIONS---------------'
+    puts 'The first player is X'
+    puts 'Choose numbers from 1 to 9 to select desired cell'
+    puts 'No duplicate numbers are allowed'
+    puts '--------------------------------'
 
-  def start
-    user_detail = user_info
-    puts "Let's Play!"
-    turn(user_detail[3], user_detail[2]) until over?
-    result(user_detail[0], user_detail[1], user_detail[2])
-  end
+    puts 'Player 1 please enter your name: '
+    p1 = gets.chomp
 
-  def user_info
-    puts 'Hello! Welcome to Tic-Toc-Toe Game'
-    puts 'What is your name Player One?'
-    player1 = gets.chomp.upcase
-    puts 'What is your name Player Two?'
-    player2 = gets.chomp.upcase
-    puts "#{player1} Please choose your symbol between X or O"
-    player1_symbol = gets.chomp
-    until player1_symbol.include?('X') || player1_symbol.include?('O')
-      if player1_symbol != 'X' || player1_symbol != 'O'
-        puts 'Wrong symbol choosen please try again'
-        player1_symbol = gets.chomp
-      end
-    end
-    player2_symbol = player1_symbol.include?('X') ? 'O' : 'X'
-    [player1, player2, player1_symbol, player2_symbol]
+    puts 'Player 2 please enter your name: '
+    p2 = gets.chomp
+
+    puts "Player 1: #{p1}, Token: X"
+    puts "Player 2: #{p2}, Token: O"
+
+    p "Here's the board"
+    display_board
   end
 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts ' ----------- '
+
+    puts ' ---------- '
+
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts ' ----------- '
+
+    puts ' ---------- '
+
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index(input)
-    input.to_i - 1
-  end
-
-  def move(position, token = 'X')
-    @board[position] = token
-  end
-
-  def position_taken?(input)
-    if @board[input] == 'X' || @board[input] == 'O'
-      true
-    else
-      false
-    end
-  end
-
-  def valid_move?(input)
-    input.between?(0, 8) && !position_taken?(input)
-  end
-
-  def turn(play1, play2)
-    puts 'Choose a spot between 1-9'
+  def turn
+    current = @game.current_player
+    puts "Player #{current}, choose a spot between 1-9"
     spot = gets.strip
-    spot = input_to_index(spot)
-    if valid_move?(spot)
-      move(spot, current_player(play1, play2))
+    spot = @game.input_to_index(spot)
+    if spot.between?(0, 8) == false
+      puts 'Please enter a valid input between 1-9'
+      return turn
+    elsif @game.valid_move?(spot)
+      @game.move(spot, @game.current_player)
     else
-      turn(play1, play2)
+      puts 'This spot has been taken choose another spot'
+      return turn
     end
     display_board
   end
 
-  def turn_count
-    count = 0
-    @board.each do |i|
-      if i == 'X'
-        count += 1
-      elsif i == 'O'
-        count += 1
-      end
-    end
-    count
-  end
-
-  def current_player(playe1, playe2)
-    turn_count.even? ? playe2 : playe1
-  end
-
-  def won?
-    WIN_COMBINATIONS.detect do |combo|
-      @board[combo[0]] == @board[combo[1]] &&
-        @board[combo[1]] == @board[combo[2]] &&
-        position_taken?(combo[0])
-    end
-  end
-
-  def full?
-    turn_count == 9
-  end
-
-  def draw?
-    !won? && full?
-  end
-
-  def over?
-    won? || full? || draw?
-  end
-
-  def winner
-    winner = won?
-    @board[winner.first]
-  end
-
-  def result(player1, player2, p1_symbol)
-    if won?
-      congo = winner
-      if congo == p1_symbol
-        puts "Congratulations #{player1} you won!"
-      else
-        puts "Congratulations #{player2} you won!"
-      end
-    elsif draw?
-      puts 'Game draw!'
+  def play
+    turn until @game.over?
+    if @game.won?
+      puts "Congratulations #{@board[@game.won?.first]}! You Won"
+    elsif @game.draw?
+      puts "It's a draw!"
     end
   end
 end
-game = Game.new
-game.start
+
+game = User.new
+game.welcome
+game.play
---
+++ spec/board_spec.rb
@@ -0,0 +1,47 @@
+require './lib/board.rb'
+
+describe Board do
+  let(:board) {Board.new}
+
+  describe '#input_to_index' do
+    it "expect an integer and decrease it by one" do
+    expect(board.input_to_index(1)).to eql(0)
+    end
+  end
+  describe "#turn_count" do
+    it "return a integer value" do
+      expect(board.turn_count).to eql(0)
+    end
+  end
+
+  describe "#current_player" do
+    it "return X" do
+      expect(board.current_player).to eql('X')
+    end
+  end
+
+  describe "#position_taken?(input)" do
+    it "return false if Board position not occupied" do
+      expect(board.position_taken?(1)).to eql(false)
+    end
+  end
+
+  describe "#valid_move?(input)" do
+    it "return true if input  between 0 to 8 and position_taken is false" do
+      expect(board.valid_move?(1)).to eql(true)
+    end
+  end
+
+  describe "#full?" do
+    it "return false if turn_count is not equal to 9" do
+      expect(board.full?).to eql(false)
+    end
+  end
+
+  describe "#over?" do
+    it "return false if not true for  won?, full? and draw?" do
+      expect(board.over?).to eql(false)
+    end
+  end
+  
+end
