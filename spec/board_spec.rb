8702f0abdf56fb0496d6cd8f131fab5b356c6a44
Hemant soni <sonihemant4741@gmail.com>
2020-05-11 23:17:08 +0530

    required changes made

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,4 @@
-require './lib/board.rb'
+require_relative '../lib/board.rb'
 
 describe Board do
   let(:board) { Board.new }
@@ -6,6 +6,9 @@ describe Board do
   describe '#input_to_index' do
     it 'expect an integer and decrease it by one' do
       expect(board.input_to_index(1)).to eql(0)
+    end
+    it 'expect an integer and decrease it by one' do
+      expect(board.input_to_index(9)).to eql(8)
     end
   end
   describe '#turn_count' do
@@ -28,19 +31,25 @@ describe Board do
 
   describe '#valid_move?(input)' do
     it 'return true if input  between 0 to 8 and position_taken is false' do
-      expect(board.valid_move?(1)).to eql(true)
+      expect(board.valid_move?(1)).not_to be false
+    end
+    it 'return false if input is not between 0 to 8 and position_taken is false' do
+      expect(board.valid_move?(9)).not_to be true
+    end
+    it 'return false if input is  between 0 to 8 and position_taken is true' do
+      expect(board.valid_move?(9)).not_to be true
     end
   end
 
   describe '#full?' do
     it 'return false if turn_count is not equal to 9' do
-      expect(board.full?).to eql(false)
+      expect(board.full?).not_to be true
     end
   end
 
   describe '#over?' do
     it 'return false if not true for  won?, full? and draw?' do
-      expect(board.over?).to eql(false)
+      expect(board.over?).not_to be true
     end
   end
 end
